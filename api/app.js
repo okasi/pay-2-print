577a538f6a2e6ddcf8e6ab71f132236a7fba030c
okasi <okans@live.se>
2019-09-20 20:38:13 +0200

    Simplified backend, serve frontend static files via express, post file & store.

--- api/app.js
+++ api/app.js
@@ -1,42 +1,51 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const express = require("express");
+const path = require("path");
+const multer = require("multer");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const exec = require("child_process").exec;
+require("dotenv").config();
 
-var indexRouter = require("./routes/index");
-
-var app = express();
-
-const printer = require("node-native-printer");
-
+const app = express();
+app.use(cors("*"));
+app.use(cookieParser());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../view/out")));
 
-app.use("/", indexRouter);
+const port = process.env.PORT || "3000";
+const http = require("http").Server(app);
+http.listen(port, () => console.log(`listening on port ${port}`));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, "upload");
+  },
+  filename: function(req, file, cb) {
+    const fileName = Date.now() + "-" + file.originalname;
+    cb(null, fileName);
+    console.log("trying to print: " + fileName);
+    exec(`lp ./upload/${fileName}`, (error, stdout, stderr) => {
+      error ? console.log("Error: " + error) : null;
+      stdout ? console.log("stdout: " + stdout) : null;
+      stderr ? console.log("stderr: " + stderr) : null;
+    });
+  }
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+const upload = multer({ storage: storage }).single("file");
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+app.post("/upload", function(req, res) {
+  upload(req, res, function(err) {
+    if (err instanceof multer.MulterError) {
+      return res.status(500).json(err);
+    } else if (err) {
+      return res.status(500).json(err);
+    }
+    return res.status(200).send(req.file);
+  });
 });
-
-//Works only with node 8
-//https://www.npmjs.com/package/node-native-printer
-
-console.log(listPrinters());
 
 module.exports = app;
